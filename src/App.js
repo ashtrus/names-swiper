24420462dd76164473a905c2b796a66004d1c5dd
ashtrus <zygimantas.butas@gmail.com>
2019-02-24 21:36:24 +0100

    removed unused texts

--- src/App.js
+++ src/App.js
@@ -27,11 +27,6 @@ import {
   View
 } from "native-base";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android: "Double tap R on your keyboard to reload,\n" + "Shake or press menu button for dev menu"
-});
-
 type Props = {};
 export default class App extends Component<Props> {
   constructor(props) {
